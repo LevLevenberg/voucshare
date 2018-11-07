097947140269618b2579680033be6f451a70aeaf
LevLevenberg <lev.personal.contact@gmail.com>
2018-11-07 17:17:49 +0200

    comm

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(!dev){
     app.use(express.static(path.resolve(__dirname, 'build')));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build/index.js'));
+        res.sendFile(path.resolve('./client/build/index.html'));
     })
 }
 
